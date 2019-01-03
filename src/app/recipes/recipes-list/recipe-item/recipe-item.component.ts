884a6a7df7bb1ba69f058c89bf11463d125dcb4c
Ashish Lakhera <Ashish.Lakhera@Evalueserve.com>
2019-01-03 18:05:08 +0530

    Hi

---
+++ src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../../recipe.model';
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent implements OnInit {
+  recipes:Recipe[]=[
+    new Recipe('A Test Recipe','This is good Recipe',
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
