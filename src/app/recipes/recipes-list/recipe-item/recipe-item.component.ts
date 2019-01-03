8564e245d90ed07041f41ed5982ba57915caa00b
Ashish Lakhera <Ashish.Lakhera@Evalueserve.com>
2019-01-03 18:45:05 +0530

    ListBind

--- src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {Recipe} from '../../recipe.model';
+
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
+
 export class RecipeItemComponent implements OnInit {
   recipes:Recipe[]=[
     new Recipe('A Test Recipe','This is good Recipe',
